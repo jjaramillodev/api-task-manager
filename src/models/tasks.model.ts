094feb9191931c5768f22365c2b528aaab16fffa
Jack Jaramillo <jjaramillo.dev@gmail.com>
2025-02-21 16:44:54 -0500

    feat(tasks model): agregar función para obtener todas las tareas con su estado

--- src/models/tasks.model.ts
+++ src/models/tasks.model.ts
@@ -1,5 +1,17 @@
+import { TaskWithStateI } from '@interfaces/task-response.interface'
+import { State } from 'src/schemas/state.schema'
+import { Task } from 'src/schemas/task.schema'
+
 export class TasksModel {
-	getTasks = async () => {
-		return []
+	getTasks = async (): Promise<TaskWithStateI[]> => {
+		const tasks = await Task.findAll({
+			attributes: ['id', 'title', 'expiration_date'],
+			include: {
+				model: State,
+				attributes: ['title', 'description'],
+				as: 'state'
+			}
+		})
+		return tasks as unknown as TaskWithStateI[]
 	}
 }
