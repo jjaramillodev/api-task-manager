edee71baad0ac0e8f5720df82862027b0e62e67c
Jack Jaramillo <jjaramillo.dev@gmail.com>
2025-02-21 16:43:55 -0500

    fix(task-response): cambiar el tipo de dato que se enviaba. - Añadir state a todas las respuestas de join.

--- src/interfaces/task-response.interface.ts
+++ src/interfaces/task-response.interface.ts
@@ -1,7 +1,15 @@
-import { Task } from 'src/schemas/task.schema'
+export interface TaskWithStateI {
+	id: number
+	title: string
+	expiration_date: Date
+	state: {
+		title: string
+		description: string
+	}
+}
 
 export interface TaskResponseI {
 	message: string
-	task?: Task
-	tasks?: Task[]
+	task?: TaskWithStateI
+	tasks?: TaskWithStateI[]
 }
