e8d6be67c19d185cfe9303d58f3f3dafa4f84601
Jack Jaramillo <jjaramillo.dev@gmail.com>
2025-02-22 19:28:12 -0500

    fix(sequelize): habilitar SSL en la configuración de Sequelize

--- src/core/sequelize.ts
+++ src/core/sequelize.ts
@@ -3,5 +3,6 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from './config'
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 	host: DB_HOST,
-	dialect: 'postgres'
+	dialect: 'postgres',
+	ssl: true
 })
